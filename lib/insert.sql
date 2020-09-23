adca1cc346806d09e89c4d70891367ad02caa89f
mike <mikesapienzaaudio@gmail.com>
2020-09-22 20:05:21 -0500

    finished

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr.Chocolate",1,"M","brown","mellow",true),
+ ("Rowdy",2,"F","Albino","aggressive",true),
+ ("Tabitha",8,"F","Black","mellow",true),
+ ("Sergeant Brown",2,"M","Brown","aggressive",false),
+  ("Melissa",3,"M","Brown","defensive",true),
+  ("Grinch",1,"M","Black","mellow",false),
+  ("Wendy",4,"M","Albino","aggressive",true),
+  (NULL,NULL,NULL,"brown","aggressive",true);
